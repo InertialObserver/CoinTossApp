2e5ce630e2d652725f4367fae855e229b456677a
Jessi Iler <inertial.observer@gmail.com>
2019-11-15 20:58:37 -0800

    program works as intended, however the design does not

--- main.py
+++ main.py
@@ -1,10 +1,16 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-import random
+
 
 class CoinTossBoxLayout(BoxLayout):
-    def choice(self,guess):
-        output="You clicked the "+guess+" button!"
-        self.ids.result.text=output
+    def choice(self, guess):
+        output = "You clicked the " + guess + " button!"
+        self.ids.result.text = output
 
 
+class CoinTossApp(App):
+    def build(self):
+        return CoinTossBoxLayout()
+
+if __name__ == "__main__":
+    CoinTossApp().run()
